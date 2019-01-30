616ca779db1ca96b77ae1c068b03acb2c6ee539f
Maciej Zrobek <maciej.zrobek@pl.ibm.com@P40227-MBP.local>
2019-01-30 16:42:57 +0100

    This is my first commit

---
+++ IIC.sh
@@ -0,0 +1,7 @@
+scutil --nc start "IIC"
+sleep 10
+sudo route delete default -interface $1
+APP="$(ifconfig $1 | grep "inet " | cut -d: -f2 | awk '{print $2}')"
+#DG="$(route -n get default | grep 'gateway' | awk '{print $2}')"
+sudo route add 10.110.0.1/16 $APP
+sudo route add 0.0.0.0 9.147.100.1
