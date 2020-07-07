a244d34e1483c77b9b288f62876c1b4b37ecc0d0
Maciej Zrobek <maciej.zrobek@pl.ibm.com@P40227-MBP.local>
2020-07-07 22:10:41 +0200

    AAAAAA

--- IIC.sh
+++ IIC.sh
@@ -6,4 +6,4 @@ APP="$(ifconfig $1 | grep "inet " | cut -d: -f2 | awk '{print $2}')"
 sudo route add 10.110.0.1/16 $APP
 sudo route add 0.0.0.0 9.147.100.1
 # This is a comment
-# This is another comment
+# This is another comment.
